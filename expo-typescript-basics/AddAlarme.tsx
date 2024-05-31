ce62f80892889c5697b734521ac47d3dd19fce38
jabsonDevElias <jabsonelias@gmail.com>
2024-05-31 19:17:54 -0300

    feat: Já esta adicionado valores no input.

--- expo-typescript-basics/AddAlarme.tsx
+++ expo-typescript-basics/AddAlarme.tsx
@@ -4,31 +4,69 @@ import { Avatar, HStack, Center, NativeBaseProvider, Box, AddIcon, StatusBar, Te
 import { Platform } from 'react-native';
 import { Feather, Octicons, MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function AddAlarme() {
+
+export default function AddAlarme(dateTimeStr: any) {
+
+
+  function formata_hora(dateStr: any) {
+    const dateObj = new Date(dateStr);
+
+    const hours = String(dateObj.getUTCHours()).padStart(2, '0');
+    const minutes = String(dateObj.getUTCMinutes()).padStart(2, '0');
+    const seconds = String(dateObj.getUTCSeconds()).padStart(2, '0');
+
+    const formattedTime = `${hours}:${minutes}:${seconds}`;
+
+    return formattedTime;
+  }
+
+  function formata_data(dateStr: any) {
+
+    const dateObj = new Date(dateStr);
+
+    const year = dateObj.getFullYear();
+    const month = String(dateObj.getMonth() + 1).padStart(2, '0');
+    const day = String(dateObj.getDate()).padStart(2, '0');
+
+    const formattedDate = `${day}/${month}/${year}`;
+
+    return formattedDate;
+  }
+
   const [date, setDate] = useState(new Date());
-  const [dateTime, setDateTime] = useState(new Date());
-  const [showDatePickerTime, setShowDatePickerTime] = useState(false);
+
+  const [data, setData] = useState("");
+  const [time, setTime] = useState("");
+
+  const [mode, setMode] = useState();
+  const [display, setDisplay] = useState();
   const [showDatePicker, setShowDatePicker] = useState(false);
 
-  const onChangeTime = (event: any, selectedDate: any) => {
-    const currentDate = selectedDate || date;
-    setShowDatePickerTime(Platform.OS === 'ios');
-    setDateTime(currentDate);
-  };
+
 
   const onChange = (event: any, selectedDate: any) => {
+
     const currentDate = selectedDate || date;
+
     setShowDatePicker(Platform.OS === 'ios');
+
     setDate(currentDate);
+    
+    if(mode == "date"){
+      const filtro_data = formata_data(currentDate);
+      setData(filtro_data);
+    }else{
+      const filtro_hora = formata_hora(currentDate);
+      setTime(filtro_hora);
+    }
   };
 
-  const showMode = () => {
+  const showMode = (mode: any, display: any) => {
+    setMode(mode);
+    setDisplay(display);
     setShowDatePicker(true);
   };
 
-  const showModeTime = () => {
-    setShowDatePickerTime(true);
-  };
   return (
     <NativeBaseProvider>
       <Box flex={1} bg="#fff" flexDirection="column" padding="5">
@@ -52,49 +90,37 @@ export default function AddAlarme() {
           <DateTimePicker
             testID="dateTimePicker"
             value={date}
-            mode="date"
+            mode={mode}
+            data-mode={mode}
             is24Hour={true}
-            display="calendar"
+            display={display}
             onChange={onChange}
           />
 
         )}
 
-        {showDatePickerTime && (
-          <DateTimePicker
-            testID="dateTimePicker2"
-            value={dateTime}
-            mode="time"
-            is24Hour={true}
-            display="clock"
-            onChange={onChangeTime}
-            
-            
-          />
-
-        )}
 
 
         <Box flex={1} flexDirection="column" width="100%" flexWrap="wrap" marginTop="59">
-          <Box  flexDirection="row" justifyContent="space-between" >
-            <Input type="text" width="75%" isDisabled value="dd/mm/aaaa" fontSize="25"></Input>
-            <Button onPress={showMode} backgroundColor="#ffcc00" width="20%">
+          <Box flexDirection="row" justifyContent="space-between" >
+            <Input type="text" width="75%" isDisabled value={data} placeholder="dd/mm/aaaa" fontSize="25"></Input>
+            <Button onPress={() => showMode('date', 'calendar')} backgroundColor="#ffcc00" width="20%" data-mode="time" data-display="clock">
               <MaterialCommunityIcons name="clock" size={30} color="black" />
             </Button>
           </Box>
 
           <Box flexDirection="row" justifyContent="space-between" marginTop="5">
-            <Input type="text" width="75%" isDisabled value="hh:mm" fontSize="25"></Input>
-            <Button onPress={showMode} backgroundColor="#ffcc00" width="20%">
+            <Input type="text" width="75%" isDisabled value={time} placeholder="hh:mm" fontSize="25"></Input>
+            <Button onPress={() => showMode('time', 'clock')} backgroundColor="#ffcc00" width="20%" data-mode="date" data-display="calendar">
               <MaterialCommunityIcons name="calendar" size={30} color="black" />
             </Button>
           </Box>
 
           <Box flexDirection="row" justifyContent="space-between" width="100%" marginTop="10">
-           <Button  backgroundColor="#ffcc00" width="80%" margin="auto">
-               <Text>Salvar</Text>
+            <Button backgroundColor="#ffcc00" width="80%" margin="auto">
+              <Text>Salvar</Text>
             </Button>
-            </Box>
+          </Box>
         </Box>
 
 
